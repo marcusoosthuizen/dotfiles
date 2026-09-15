d7fd587dd96e88212a964940d735655653636906
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-09-15 13:20:58 +0800

    fix pywalfox path & remove noctalia theming

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -2,13 +2,10 @@
 
 # pywalfox
 pipx install pywalfox
-pywalfox install --manifest-path ~/.mozilla/native-messaging-hosts \
+.local/bin/pywalfox install --manifest-path ~/.mozilla/native-messaging-hosts \
                  --profile-path  ~/.config/librewolf/librewolf
 
 # default theming
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
-
-# noctalia color scheme
-noctalia msg wallpaper-set /usr/share/backgrounds/steins.png
