2486bc9e388d06ca9d736ca68742f7e7c5f3753b
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-09-15 16:02:32 +0800

    removed reboot from startup.sh

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -23,6 +23,3 @@ mkdir -p ".steam/steam/ubuntu12_32" ".steam/steam/ubuntu12_64"
 ln -sf /usr/lib/millennium/libmillennium_bootstrap_x86.so   ".steam/steam/ubuntu12_32/libXtst.so.6"
 ln -sf /usr/lib/millennium/libmillennium_bootstrap_hhx64.so ".steam/steam/ubuntu12_64/libXtst.so.6"
 ln -sf /usr/lib/millennium/libmillennium_hhx64.so           ".steam/steam/ubuntu12_64/libmillennium_hhx64.so"
-
-# reboot machine
-reboot
