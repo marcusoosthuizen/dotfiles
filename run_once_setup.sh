d20031120a7b22929f18cbdbbd4f7db276668ade
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-09-15 14:10:20 +0800

    creating default directories in setup script

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -10,5 +10,8 @@ gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
+# creating default dirs
+xdg-user-dirs-update
+
 # reboot machine
 reboot
