810b5b46cce3da173759155eeb1fb4e21020d07a
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-09-15 12:42:01 +0800

    moved papirus folder colors to iso

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -10,8 +10,5 @@ gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
-# papirus folders
-papirus-folders -t Papirus-Dark -C yaru
-
 # noctalia color scheme
 noctalia msg wallpaper-set /usr/share/backgrounds/steins.png
