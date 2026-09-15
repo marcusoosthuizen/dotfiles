1f39937c436244eed14aedb0d620b7612d53d635
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-09-15 10:09:53 +0800

    Create run_once_setup.sh

---
+++ run_once_setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# pywalfox
+pipx install pywalfox
+pywalfox install --manifest-path ~/.mozilla/native-messaging-hosts \
+                 --profile-path  ~/.config/librewolf/librewolf
+
+# default theming
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+
+# papirus folders
+papirus-folders -t Papirus-Dark -C yaru
+
+# noctalia color scheme
+noctalia msg wallpaper-set /usr/share/backgrounds/steins.png
