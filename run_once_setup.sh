207df27d98edbae673bfc56e35fee781f79df477
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-09-15 13:50:46 +0800

    test reboot

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -9,3 +9,6 @@ pipx install pywalfox
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+
+# reboot machine
+reboot
