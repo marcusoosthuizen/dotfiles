6859bf6ea0a3b0bd01b19f1148396d1fea3e5ccf
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-09-15 15:33:20 +0800

    added millenium post install commands

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -13,5 +13,16 @@ gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 # creating default dirs
 xdg-user-dirs-update
 
+# steam millenium post install
+beta_file=".steam/steam/package/beta"
+if [ -f "${beta_file}" ]; then
+    log "removing beta '$(cat "${beta_file}")' in favor for stable."
+    rm "${beta_file}"
+fi
+mkdir -p ".steam/steam/ubuntu12_32" ".steam/steam/ubuntu12_64"
+ln -sf /usr/lib/millennium/libmillennium_bootstrap_x86.so   ".steam/steam/ubuntu12_32/libXtst.so.6"
+ln -sf /usr/lib/millennium/libmillennium_bootstrap_hhx64.so ".steam/steam/ubuntu12_64/libXtst.so.6"
+ln -sf /usr/lib/millennium/libmillennium_hhx64.so           ".steam/steam/ubuntu12_64/libmillennium_hhx64.so"
+
 # reboot machine
 reboot
