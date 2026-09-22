d3583fa11037578af4c7b06a10eba38ac2e19f25
marcusoosthuizen <112798399+marcusoosthuizen@users.noreply.github.com>
2026-09-22 15:51:39 +0800

    added Zed flatpak & updated niri config

--- run_once_flatpaks.sh
+++ run_once_flatpaks.sh
@@ -6,6 +6,7 @@ flatpak install -y org.gtk.Gtk3theme.adw-gtk3 # gtk themes
 flatpak install -y flathub page.codeberg.libre_menu_editor.LibreMenuEditor # menu editor
 
 flatpak install -y flathub app.zen_browser.zen # browser
+flatpak install -y flathub dev.zed.Zed # code editor
 flatpak install -y flathub org.gnome.EasyTAG # tagging software
 flatpak install -y flathub com.obsproject.Studio # recording software
 flatpak install -y flathub org.gimp.GIMP # art program
@@ -14,5 +15,5 @@ flatpak install -y flathub dev.vencord.Vesktop # discord client
 # flatpak overrides
 flatpak override --user --filesystem=xdg-config/gtk-3.0:ro --filesystem=xdg-config/gtk-4.0:ro # gtk theming\
 flatpak override --user --socket=wayland # making flatpaks open in wayland
-#flatpak override --user app.zen_browser.zen --filesystem=xdg-home/.zen # zen theme
+flatpak override --user dev.zed.Zed --filesystem=xdg-config/zed # zed theme
 flatpak override --user com.obsproject.Studio --filesystem=xdg-config/obs-studio # obs theme
